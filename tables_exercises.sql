3edaf72c525f7b17c21bafc147f920d414bbc873
01Codie10 <pedrojobs11@gmail.com>
2022-07-05 12:16:44 -0500

    My SQL table exercises for Codeup.

---
+++ tables_exercises.sql
@@ -0,0 +1,17 @@
+SHOW TABLES;
+
+-- 6.) Which table(s) do you think contain a numeric type column? 
+-- employee number
+
+-- 7.) Which table(s) do you think contain a string type column?
+-- first/last name, gender
+
+-- 8.)Which table(s) do you think contain a date type column?
+-- birth date, hire date
+
+-- 9.) What is the relationship between the employees and the departments tables?
+-- None
+
+SHOW CREATE TABLE dept_manager;
+SHOW TABLES;
+
